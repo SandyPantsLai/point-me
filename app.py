9827165ec75148f2fc17182f2133a2b2a3393149
Sandy Lai <sandy.lai@sandylai-ltm.internal.salesforce.com>
2019-07-15 18:15:48 -0400

    Add main route back.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 app = Flask( __name__ )
 
-@app.route('/blue/')
+@app.route('/')
 def main():
+    return 'Redirected to Blue from /!'
+
+@app.route('/blue/')
+def blue():
     return 'Blue!'
